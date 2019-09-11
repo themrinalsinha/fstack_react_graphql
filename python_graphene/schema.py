2019a9d1953cd623d8e6d5aa936e7037bcff3985
Mrinal Sinha <themrinalsinha@gmail.com>
2019-09-11 16:35:18 +0530

    working with mutation

--- python_graphene/schema.py
+++ python_graphene/schema.py
@@ -1,13 +1,14 @@
 import graphene
+from uuid import uuid4
 from datetime import datetime
 
 # --------------------------------------------------------
 # Now let's say we have some complex data, like user data
 
 class User(graphene.ObjectType):
-    id         = graphene.ID()
+    id         = graphene.ID(default_value=str(uuid4()))
     username   = graphene.String()
-    created_at = graphene.DateTime()
+    created_at = graphene.DateTime(default_value=datetime.now())
 
 # to pass the limit parameter
 class Query(graphene.ObjectType):
@@ -28,7 +29,25 @@ class Query(graphene.ObjectType):
             User(id='3', username='Pred', created_at=datetime.now()),
         ][:limit]
 
-schema = graphene.Schema(query=Query, auto_camelcase=False) # disable auto_camelcase or you have to pass is_admin as isAdmin
+# working/creating mutation
+class CreateUser(graphene.Mutation):
+    # since, we want to return our entire user.
+    user = graphene.Field(User)
+
+    class Arguments:
+        username = graphene.String()
+
+    def mutate(self, info, username):
+        # user = User(id='3', username=username, created_at=datetime.now())
+        user = User(username=username)
+        return CreateUser(user=user)
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation) # disable auto_camelcase or you have to pass is_admin as isAdmin
 result = schema.execute(
     '''
     {
@@ -39,18 +58,44 @@ result = schema.execute(
 result_1 = schema.execute(
     '''
     {
-        is_admin
+        isAadmin
     }
     '''
 )
 
+# result_users = schema.execute(
+#     '''
+#     {
+#         users (limit: 2) {
+#             id
+#             username
+#             created_at
+#         }
+#     }
+#     '''
+# ) # with limit
+
 result_users = schema.execute(
     '''
     {
-        users (limit: 2) {
+        users {
             id
             username
-            created_at
+            createdAt
+        }
+    }
+    '''
+)
+
+res_mutate = schema.execute(
+    '''
+    mutation {
+        createUser(username: "Hefner") {
+            user {
+                id
+                username
+                createdAt
+            }
         }
     }
     '''
@@ -62,12 +107,16 @@ import json
 result_json = dict(result.data.items())
 print(json.dumps(result_json, indent=4))
 
-print(result_1.data.items())
-
-print(result_users.data.items())
-print(json.dumps(result_users.data, indent=4))
-
-
 # --------------------------------------------------------------
 # eg: say wanted to know if a user was an administrator or not.
 # we might have an is admin query.
+
+# print(result_1.data.items())
+
+# print(result_users.data.items())
+# print(json.dumps(result_users.data, indent=4))
+
+
+print(json.dumps(res_mutate.data, indent=4))
+print('\n------------------------------------------')
+print(json.dumps(result_users.data, indent=4))
