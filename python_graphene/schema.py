80c0defb398fb008408efa7357ea9752569f2f6b
Mrinal Sinha <themrinalsinha@gmail.com>
2019-09-11 13:19:14 +0530

    handling boolean field and schema issues

--- python_graphene/schema.py
+++ python_graphene/schema.py
@@ -2,15 +2,26 @@ import graphene
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
+    is_admin = graphene.Boolean()
 
     def resolve_hello(self, info):
         return 'world'
 
-schema = graphene.Schema(query=Query)
+    def resolve_is_admin(self, info):
+        return True
+
+schema = graphene.Schema(query=Query, auto_camelcase=False) # disable auto_camelcase or you have to pass is_admin as isAdmin
 result = schema.execute(
     '''
     {
         hello
+    }
+    '''
+)
+result_1 = schema.execute(
+    '''
+    {
+        is_admin
     }
     '''
 )
@@ -20,3 +31,9 @@ print(result.data.items())
 import json
 result_json = dict(result.data.items())
 print(json.dumps(result_json, indent=4))
+
+print(result_1.data.items())
+
+# --------------------------------------------------------------
+# eg: say wanted to know if a user was an administrator or not.
+# we might have an is admin query.
